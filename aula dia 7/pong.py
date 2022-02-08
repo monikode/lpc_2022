fe6f656468961854c82e19ec6d825239fe3deb56
Monike Freitas <monike.ftsousa@gmail.com>
2022-02-07 20:04:09 -0400

    creating pong

---
+++ aula dia 7/pong.py
@@ -0,0 +1,48 @@
+import turtle
+import os
+
+screen = turtle.Screen()
+screen.title("My Pong")
+screen.bgcolor("black")
+screen.setup(width=800, height=800)
+screen.tracer(0)
+
+paddle_1 = turtle.Turtle()
+paddle_1.speed(0)
+paddle_1.shape("square")
+paddle_1.color("white")
+paddle_1.shapesize(stretch_wid=5, stretch_len=1)
+paddle_1.penup()
+paddle_1.goto(-350, 0)
+
+paddle_2 = turtle.Turtle()
+paddle_2.speed(0)
+paddle_2.shape("square")
+paddle_2.color("white")
+paddle_2.shapesize(stretch_wid=5, stretch_len=1)
+paddle_2.penup()
+paddle_2.goto(350, 0)
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 1
+ball.dy = 1
+
+score_1 = 0
+score_2 = 0
+
+hud = turtle.Turtle()
+hud.speed(0)
+hud.shape("square")
+hud.color("white")
+hud.penup()
+hud.hideturtle()
+hud.goto(0, 260)
+hud.write("0 : 0", align="center", font=("Press Start 2P", 24, "normal"))
+
+while True:
+    screen.update()
