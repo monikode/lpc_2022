6fcdccd444965b4f3b7fe54f2534fb70a359b0ff
Monike Freitas <monike.ftsousa@gmail.com>
2022-02-07 20:32:07 -0400

    update pong

--- aula dia 7/pong.py
+++ aula dia 7/pong.py
@@ -32,9 +32,6 @@ ball.goto(0, 0)
 ball.dx = 1
 ball.dy = 1
 
-score_1 = 0
-score_2 = 0
-
 hud = turtle.Turtle()
 hud.speed(0)
 hud.shape("square")
@@ -43,6 +40,49 @@ hud.penup()
 hud.hideturtle()
 hud.goto(0, 260)
 hud.write("0 : 0", align="center", font=("Press Start 2P", 24, "normal"))
+
+
+def paddle_1_up():
+    y = paddle_1.ycor()
+    if y < 250:
+        y += 30
+    else:
+        y = 250
+    paddle_1.sety(y)
+
+
+def paddle_1_down():
+    y = paddle_1.ycor()
+    if y > -250:
+        y += -30
+    else:
+        y = -250
+    paddle_1.sety(y)
+
+
+def paddle_2_up():
+    y = paddle_2.ycor()
+    if y < 250:
+        y += 30
+    else:
+        y = 250
+    paddle_2.sety(y)
+
+
+def paddle_2_down():
+    y = paddle_2.ycor()
+    if y > -250:
+        y += -30
+    else:
+        y = -250
+    paddle_2.sety(y)
+
+
+screen.listen()
+screen.onkeypress(paddle_1_up, "w")
+screen.onkeypress(paddle_1_down, "s")
+screen.onkeypress(paddle_2_up, "Up")
+screen.onkeypress(paddle_2_down, "Down")
 
 while True:
     screen.update()
